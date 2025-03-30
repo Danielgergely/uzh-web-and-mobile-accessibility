17d9d9fc23951febdd23b2c41056603d3eb83e83
Raphael van den Doel <doelraphael@gmail.com>
2025-03-30 11:11:10 +0200

    Update login form

--- src/js/login.js
+++ src/js/login.js
@@ -25,6 +25,23 @@ function removeValidation(element) {
     element.classList.remove('is-invalid');
 }
 
+function updateErrorMessage($errorContainer, errorCount) {
+    if (errorCount === 0) {
+        $errorContainer.addClass('d-none');
+        return;
+    }
+
+    // Make error container visible
+    $errorContainer.removeClass('d-none');
+
+    // Set appropriate message based on error count
+    if (errorCount === 1) {
+        $errorContainer.html('<h3 class="h5 m-0">Please correct the following error:</h3>');
+    } else {
+        $errorContainer.html('<h3 class="h5 m-0">Please correct the following ' + errorCount + ' errors:</h3>');
+    }
+}
+
 /**
  * Validate the login form and try to log the user in
  * @param {object} event - The DOM event
@@ -33,9 +50,10 @@ function login(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    var hasError = false;
+    var errorCount = 0;
 
     var $errorList = $('#login-error-list');
+    var $errorContainer = $('#login-error');
     $errorList.empty();
 
     $('.invalid-feedback').empty();
@@ -49,12 +67,12 @@ function login(event) {
         setInvalid(email);
         $emailFeedback.text('Email is required');
         $errorList.append('<li><a href="#login-email-control">Email is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setInvalid(email);
         $emailFeedback.text('Please enter a valid email address');
         $errorList.append('<li><a href="#login-email-control">Please enter a valid email address</a></li>');
-        hasError = true;
+        errorCount++;
     }
 
     var password = document.getElementById('login-password-control');
@@ -63,23 +81,21 @@ function login(event) {
         setInvalid(password);
         $passwordFeedback.text('Password is required');
         $errorList.append('<li><a href="#login-password-control">Password is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setValid(password);
     }
 
-    if (hasError) {
+    updateErrorMessage($errorContainer, errorCount);
+
+    if (errorCount) {
         $('#login-error').removeClass('d-none');
         $errorList.removeClass('sr-only');
 
         $('#login-error').attr('tabindex', '-1').focus();
-
-        $('#login-login-button').find('.state-indicator').text('Form has errors');
     } else {
         $('#login-error').addClass('d-none');
         $errorList.addClass('sr-only');
-
-        $('#login-login-button').find('.state-indicator').text('');
     }
 }
 
@@ -91,7 +107,7 @@ function forgot(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    var hasError = false;
+    var errorCount = 0;
 
     var $errorList = $('#login-error-list');
     $errorList.empty();
@@ -105,30 +121,29 @@ function forgot(event) {
         setInvalid(email);
         $emailFeedback.text('Email is required for password recovery');
         $errorList.append('<li><a href="#login-email-control">Email is required for password recovery</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setInvalid(email);
         $emailFeedback.text('Please enter a valid email address');
         $errorList.append('<li><a href="#login-email-control">Please enter a valid email address</a></li>');
-        hasError = true;
+        errorCount++;
     }
 
     var password = document.getElementById('login-password-control');
     removeValidation(password);
     $(password).siblings('.invalid-feedback').empty();
 
-    if (hasError) {
+    var $errorContainer = $('#login-error');
+    updateErrorMessage($errorContainer, errorCount);
+
+    if (errorCount) {
         $('#login-error').removeClass('d-none');
         $errorList.removeClass('sr-only');
 
         $('#login-error').attr('tabindex', '-1').focus();
-
-        $('#login-forgot-button').find('.state-indicator').text('Form has errors');
     } else {
         $('#login-error').addClass('d-none');
         $errorList.addClass('sr-only');
-
-        $('#login-forgot-button').find('.state-indicator').text('');
     }
 }
 
@@ -140,8 +155,8 @@ function register(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    var hasError = false;
-    
+    var errorCount = 0;
+
     var $errorList = $('#register-error-list');
     $errorList.empty();
     $('.invalid-feedback').empty();
@@ -153,7 +168,7 @@ function register(event) {
         setInvalid(firstName);
         $firstNameFeedback.text('First name is required');
         $errorList.append('<li><a href="#register-first-name-control">First name is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (firstName.validity.valid) {
         setValid(firstName);
     }
@@ -165,7 +180,7 @@ function register(event) {
         setInvalid(lastName);
         $lastNameFeedback.text('Last name is required');
         $errorList.append('<li><a href="#register-last-name-control">Last name is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (lastName.validity.valid) {
         setValid(lastName);
     }
@@ -179,12 +194,12 @@ function register(event) {
         setInvalid(email);
         $emailFeedback.text('Email is required');
         $errorList.append('<li><a href="#register-email-control">Email is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setInvalid(email);
         $emailFeedback.text('Please enter a valid email address');
         $errorList.append('<li><a href="#register-email-control">Please enter a valid email address</a></li>');
-        hasError = true;
+        errorCount++;
     }
 
     // Password validation
@@ -195,27 +210,27 @@ function register(event) {
         setInvalid(password);
         $passwordFeedback.text('Password is required');
         $errorList.append('<li><a href="#register-password-control">Password is required</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (passwordValue.length < 8) {
         setInvalid(password);
         $passwordFeedback.text('Password must be at least 8 characters long');
         $errorList.append('<li><a href="#register-password-control">Password must be at least 8 characters long</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (passwordValue.length > 16) {
         setInvalid(password);
         $passwordFeedback.text('Password must be at most 16 characters long');
         $errorList.append('<li><a href="#register-password-control">Password must be at most 16 characters long</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (passwordValue.match(/[a-zA-Z]+/) == null) {
         setInvalid(password);
         $passwordFeedback.text('Password must contain at least one letter');
         $errorList.append('<li><a href="#register-password-control">Password must contain at least one letter</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (passwordValue.match(/[0-9]+/) == null) {
         setInvalid(password);
         $passwordFeedback.text('Password must contain at least one number');
         $errorList.append('<li><a href="#register-password-control">Password must contain at least one number</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setValid(password);
     }
@@ -227,38 +242,29 @@ function register(event) {
         setInvalid(programme);
         $programmeFeedback.text('Please select a programme');
         $errorList.append('<li><a href="#register-programme-control">Please select a programme</a></li>');
-        hasError = true;
+        errorCount++;
     } else if (!programme.validity.valid) {
         setInvalid(programme);
         $programmeFeedback.text('Please select a valid programme');
         $errorList.append('<li><a href="#register-programme-control">Please select a valid programme</a></li>');
-        hasError = true;
+        errorCount++;
     } else {
         setValid(programme);
     }
 
+    var $errorContainer = $('#register-error');
+    updateErrorMessage($errorContainer, errorCount);
+
     // Show/hide error summary
-    if (hasError) {
+    if (errorCount) {
         $('#register-error').removeClass('d-none');
         $errorList.removeClass('sr-only');
 
         // Set focus to error container for screen readers
         $('#register-error').attr('tabindex', '-1').focus();
-
-        // Update button state for screen readers
-        $('#register-register-button').find('.state-indicator').text('Form has errors');
     } else {
         $('#register-error').addClass('d-none');
         $errorList.addClass('sr-only');
-
-        // Update button state for screen readers
-        $('#register-register-button').find('.state-indicator').text('Processing registration...');
-
-        // Here you would handle the actual registration process
-        // For demonstration, let's just clear it after a moment
-        setTimeout(function() {
-            $('#register-register-button').find('.state-indicator').text('');
-        }, 2000);
     }
 }
 
